3ecfc6c4f7c3fe968eef8b4b5691772de2e90afe
nangongxiaoxin <57568897+nangongxiaoxin@users.noreply.github.com>
2023-03-20 18:12:02 +0800

    20230320

--- src/components/power/Roles.vue
+++ src/components/power/Roles.vue
@@ -17,6 +17,25 @@
         </el-row>
 
         <!-- 角色列表区域 -->
+        <el-table :data="rolelist" border stripe>
+          <!-- 展开列 -->
+          <el-table-column type="expand">
+            <template slot-scope="scope">
+              {{ scope.row }}
+            </template>
+          </el-table-column>
+          <!-- 索引列 -->
+          <el-table-column type="index"></el-table-column>
+          <el-table-column label="角色名称" prop="roleName"></el-table-column>
+          <el-table-column label="角色描述" prop="roleDesc"></el-table-column>
+          <el-table-column label="操作" width="300px">
+            <template slot-scope="scope">
+              <el-button size="mini" type="primary" icon="el-icon-edit">编辑</el-button>
+              <el-button size="mini" type="danger" icon="el-icon-delete">删除</el-button>
+              <el-button size="mini" type="warning" icon="el-icon-setting">分配权限</el-button>
+            </template>
+          </el-table-column>
+        </el-table>
     </el-card>
   </div>
 </template>
