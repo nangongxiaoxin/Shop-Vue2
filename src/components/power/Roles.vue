e6e313e97c3a9d67c6df52e42f3dfa60638290f8
nangongxiaoxin <57568897+nangongxiaoxin@users.noreply.github.com>
2023-03-20 19:22:46 +0800

    202303201922

--- src/components/power/Roles.vue
+++ src/components/power/Roles.vue
@@ -9,33 +9,70 @@
 
     <!-- 卡片视图 -->
     <el-card>
-        <!-- 添加角色按钮区 -->
-        <el-row>
-            <el-col>
-                <el-button type="primary">添加角色</el-button>
-            </el-col>
-        </el-row>
+      <!-- 添加角色按钮区 -->
+      <el-row>
+        <el-col>
+          <el-button type="primary">添加角色</el-button>
+        </el-col>
+      </el-row>
 
-        <!-- 角色列表区域 -->
-        <el-table :data="rolelist" border stripe>
-          <!-- 展开列 -->
-          <el-table-column type="expand">
-            <template slot-scope="scope">
-              {{ scope.row }}
-            </template>
-          </el-table-column>
-          <!-- 索引列 -->
-          <el-table-column type="index"></el-table-column>
-          <el-table-column label="角色名称" prop="roleName"></el-table-column>
-          <el-table-column label="角色描述" prop="roleDesc"></el-table-column>
-          <el-table-column label="操作" width="300px">
-            <template slot-scope="scope">
-              <el-button size="mini" type="primary" icon="el-icon-edit">编辑</el-button>
-              <el-button size="mini" type="danger" icon="el-icon-delete">删除</el-button>
-              <el-button size="mini" type="warning" icon="el-icon-setting">分配权限</el-button>
-            </template>
-          </el-table-column>
-        </el-table>
+      <!-- 角色列表区域 -->
+      <el-table :data="rolelist" border stripe>
+        <!-- 展开列 -->
+        <el-table-column type="expand">
+          <template slot-scope="scope">
+            <el-row
+              :class="['bdbottom', i1 === 0 ? 'bdtop' : '','vcenter']"
+              v-for="(item1, i1) in scope.row.children"
+              :key="item1.id"
+              
+            >
+              <!-- 渲染一级权限 -->
+              <el-col :span="5">
+                <el-tag>
+                  {{ item1.authName }}
+                </el-tag>
+                <i class="el-icon-caret-right"></i>
+              </el-col>
+
+              <!-- 渲染二级和三级权限 -->
+              <el-col :span="19">
+                <!-- 通过for循环嵌套渲染二级权限 -->
+                <el-row :class="[i2==0?'':'bdtop','vcenter']" v-for="(item2,i2) in item1.children" :key="item2.id">
+                  <el-col :span="6">
+                    <el-tag type="success">{{ item2.authName }}</el-tag>
+                    <i class="el-icon-caret-right"></i>
+                  </el-col>
+
+                  <!-- 一共24列？？？？？？ -->
+                  <el-col :span="18">
+                    <el-tag type="warning" v-for="(item3,i3) in item2.children" :key="item3.id">{{ item3.authName }}</el-tag>
+                  </el-col>
+                </el-row>
+              </el-col>
+            </el-row>
+
+            <!-- <pre>{{ scope.row }}</pre> -->
+          </template>
+        </el-table-column>
+        <!-- 索引列 -->
+        <el-table-column type="index"></el-table-column>
+        <el-table-column label="角色名称" prop="roleName"></el-table-column>
+        <el-table-column label="角色描述" prop="roleDesc"></el-table-column>
+        <el-table-column label="操作" width="300px">
+          <template slot-scope="scope">
+            <el-button size="mini" type="primary" icon="el-icon-edit"
+              >编辑</el-button
+            >
+            <el-button size="mini" type="danger" icon="el-icon-delete"
+              >删除</el-button
+            >
+            <el-button size="mini" type="warning" icon="el-icon-setting"
+              >分配权限</el-button
+            >
+          </template>
+        </el-table-column>
+      </el-table>
     </el-card>
   </div>
 </template>
@@ -45,8 +82,8 @@ export default {
   name: 'Roles',
   data() {
     return {
-        // 所有角色列表区域数据
-        rolelist:[]
+      // 所有角色列表区域数据
+      rolelist: [],
     }
   },
   created() {
@@ -54,19 +91,36 @@ export default {
   },
   methods: {
     // 获取所有角色的列表
-    async getRolesList(){
-        const{data:res}=await this.$http.get('roles')
-        if(res.meta.status!==200){
-            return this.$message.error('获取角色列表失败')
-        }
+    async getRolesList() {
+      const { data: res } = await this.$http.get('roles')
+      if (res.meta.status !== 200) {
+        return this.$message.error('获取角色列表失败')
+      }
 
-        this.rolelist= res.data
+      this.rolelist = res.data
 
-        console.log(this.rolelist)
-    }
+      console.log(this.rolelist)
+    },
   },
 }
 </script>
 
 <style lang="less" scoped>
+.el-tag {
+  margin: 7px;
+}
+
+.bdtop {
+  border-top: 1px solid #eee;
+}
+
+.bdbottom {
+  border-bottom: 1px solid #eee;
+}
+
+// 权限显示居中
+.vcenter{
+  display: flex;
+  align-items: center;
+}
 </style>
