612b08475895163947f5b362c727500db27299d3
nangongxiaoxin <bx_tianhuan@163.com>
2023-04-03 17:35:40 +0800

    202304031735

--- src/components/goods/Add.vue
+++ src/components/goods/Add.vue
@@ -18,12 +18,60 @@
         show-icon
       ></el-alert>
 
-        <!-- 步骤条区域 -->
-      <el-steps :space="200" :active="1" finish-status="success">
-        <el-step title="已完成"></el-step>
-        <el-step title="进行中"></el-step>
-        <el-step title="步骤 3"></el-step>
+      <!-- 步骤条区域 -->
+      <el-steps
+        :space="200"
+        :active="acticveIndex - 0"
+        finish-status="success"
+        align-center
+      >
+        <el-step title="基本信息"></el-step>
+        <el-step title="商品参数"></el-step>
+        <el-step title="商品属性"></el-step>
+        <el-step title="商品图片"></el-step>
+        <el-step title="商品内容"></el-step>
+        <el-step title="完成"></el-step>
       </el-steps>
+
+      <!-- tab栏区域 -->
+      <el-form
+        :model="addForm"
+        :rules="addRormRules"
+        ref="addFormRef"
+        label-width="100px"
+        label-position="top"
+      >
+        <el-tabs :tab-position="'left'" v-model="acticveIndex">
+          <el-tab-pane label="基本信息" name="0">
+            <el-form-item label="商品名称" prop="goods_name">
+              <el-input v-model="addForm.goods_name"></el-input>
+            </el-form-item>
+            <el-form-item label="商品价格" prop="goods_price">
+              <el-input v-model="addForm.goods_price" type="number"></el-input>
+            </el-form-item>
+            <el-form-item label="商品重量" prop="goods_weight">
+              <el-input v-model="addForm.goods_weight" type="number"></el-input>
+            </el-form-item>
+            <el-form-item label="商品数量" prop="goods_number">
+              <el-input v-model="addForm.goods_number" type="number"></el-input>
+            </el-form-item>
+
+            <el-form-item label="商品分类" prop="">
+              <el-cascader
+                v-model="value"
+                :options="cateList"
+                :props="cateProps"
+                @change="handleChange"
+              >
+              </el-cascader>
+            </el-form-item>
+          </el-tab-pane>
+          <el-tab-pane label="商品参数" name="1">商品参数</el-tab-pane>
+          <el-tab-pane label="商品属性" name="2">商品属性</el-tab-pane>
+          <el-tab-pane label="商品图片" name="3">商品图片</el-tab-pane>
+          <el-tab-pane label="商品属性" name="4">商品内容</el-tab-pane>
+        </el-tabs>
+      </el-form>
     </el-card>
   </div>
 </template>
@@ -33,26 +81,57 @@ export default {
   name: 'Add',
   data() {
     return {
-      // 表单数据对象
+      acticveIndex: '0',
+      // 添加商品的表单数据对象
       addForm: {
         goods_name: '',
+        goods_price: 0,
+        goods_weight: 0,
+        goods_number: 0,
+        //商品所属的分类数组
         goods_cat: [],
-        goods_price: '',
-        goods_number: '',
-        goods_weight: '',
-        goods_introduce: '',
-        pics: [],
-        attrs: [],
       },
-      // 商品分类数据
+      // 添加商品的表单验证规则对象
+      addRormRules: {
+        goods_name: [
+          { required: true, message: '请输入商品名称', trigger: 'blur' },
+        ],
+        goods_price: [
+          { required: true, message: '请输入商品价格', trigger: 'blur' },
+        ],
+        goods_weight: [
+          { required: true, message: '请输入商品重量', trigger: 'blur' },
+        ],
+        goods_number: [
+          { required: true, message: '请输入商品数量', trigger: 'blur' },
+        ],
+      },
+      // 分类列表数据
       cateList: [],
-      // 商品分类级联选择器
+      // 级联选择器的配置对象
+      cateProps: {
+        value: 'cat_id',
+        label: 'cat_name',
+        children: 'children',
+        expandTrigger: 'hover',
+      },
     }
   },
   created() {
     this.getCateList()
   },
-  methods() {},
+  methods: {
+    // 获取分类列表数据
+    async getCateList() {
+      const { data: res } = await this.$http.get('categories')
+
+      if (res.meta.status !== 200)
+        return this.$message.error('获取分类列表失败！')
+      this.cateList = res.data
+
+      // console.log(this.cateList)
+    },
+  },
 }
 </script>
 
